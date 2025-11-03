0d59aaae9927c6153ef883f5af970b64b348faa3
yuanjasper <yuanjasper14@gmail.com>
2025-11-04 03:05:52 +0800

    Edit style check

--- src/test/java/seedu/address/logic/parser/EditHoursCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditHoursCommandParserTest.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.EditHoursCommand;
 
 public class EditHoursCommandParserTest {
